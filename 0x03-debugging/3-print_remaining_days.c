1ef6397a4eb6421a0b0dd478692fe5f20862c818
Omar <138858455+sk1nsan@users.noreply.github.com>
2023-08-16 02:45:22 -0700

    test7

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,26 +9,25 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0) && (((year % 400 == 0) && (year % 100 == 0)) || (!(year % 400 == 0) && !(year % 100 == 0)) ))
-    {
-        if (month > 2)
-        {
-            day++;
-        }
+	int y4 = year % 4 == 0, y400 = year % 400 == 0, y100 = year % 100 == 0;
 
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
-    {
-        if (month == 2 && day == 60)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-    }
+	if (y4 && ((y400 && y100) || (!y400 && !y100)))
+	{
+		if (month > 2)
+			day++;
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
 }
