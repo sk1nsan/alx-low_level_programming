67fe0a961f75607c370c599dae56f0b4c88a695b
Omar <138858455+sk1nsan@users.noreply.github.com>
2023-08-30 05:00:01 -0700

    mroe

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,11 +1,30 @@
 #include "main.h"
+/**
+ * cpystring - return a copy of s
+ *
+ * @src: string to be copied
+ * @dest: destination of the copy
+ *
+ * Return: d a copy of s
+ */
+char *cpystring(char *dest, char *src)
+{
+	int i;
 
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+
+}
 /**
  * helper - checks if the first and last chars are equal
  *
  * @s: string to be checked
  *
- * Return 1 if they are equal
+ * Return: 1 if they are equal
  * 0 otherwise
  */
 
@@ -16,10 +35,9 @@ int helper(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 		;
 	j = (s[0] == s[i - 1]);
-	s[i-1] = '\0';
+	s[i - 1] = '\0';
 	return (j);
 }
-
 
 /**
  * is_palindrome - checks if s is a palindrome
@@ -32,13 +50,13 @@ int helper(char *s)
 
 int is_palindrome(char *s)
 {
-	if (s[0] == '\0' || s[1] == '\0')
-	{
+	char t[1000];
+
+	cpystring(t, s);
+
+	if (t[0] == '\0' || t[1] == '\0')
 		return (1);
-	}
-	if (helper(s))
-	{
-		is_palindrome(s+1);
-	}
+	if (helper(t))
+		return (is_palindrome(t + 1));
 	return (0);
 }
