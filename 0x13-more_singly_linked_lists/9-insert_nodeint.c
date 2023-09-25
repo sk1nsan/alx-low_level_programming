00361bf9ec39d4385c120f21f561b4119de7fb4d
Omar <138858455+sk1nsan@users.noreply.github.com>
2023-09-25 09:10:39 -0700

    insert node at idx

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inset  node at index
+ *
+ * @head: linked list
+ * @idx: index of new node
+ * @n: int of new node
+ *
+ * Return: new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *new, *old, *itr = *head;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	while (i <= idx)
+	{
+		if (itr->next == NULL && i != idx)
+			return (NULL);
+		if (i == idx)
+		{
+			new = malloc(sizeof(listint_t));
+			old = malloc(sizeof(listint_t));
+			if (new == NULL || old == NULL)
+				return (NULL);
+			old->n = itr->n;
+			old->next = itr->next;
+			new = itr;
+			new->n = n;
+			new->next = old;
+			return (new);
+		}
+		itr = itr->next;
+		i++;
+	}
+	return (NULL);
+}
