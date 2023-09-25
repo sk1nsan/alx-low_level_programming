c39125613568c2341da58652104c6f17978834e1
Omar <138858455+sk1nsan@users.noreply.github.com>
2023-09-25 09:56:24 -0700

    fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (i <= idx)
 	{
-		if (itr->next == NULL && i == idx)
+		if (itr == NULL)
 			return (NULL);
 		if (i == idx - 1)
 		{
