8c4eabaa7a211cbede95650294aeba46a3ba2896
Omar <138858455+sk1nsan@users.noreply.github.com>
2023-09-25 11:13:48 -0700

    print list safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints all elements of linked list
+ *
+ * @head: linked list
+ *
+ * Return: Number of elements
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int n = 0, i;
+	const listint_t *x[100];
+
+	if (head == NULL)
+		exit(98);
+	while (1)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (x[i]  == head)
+				return (n);
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		x[n] = head;
+		n++;
+		if (head->next == NULL)
+			return (n);
+		head = head->next;
+	}
+}
