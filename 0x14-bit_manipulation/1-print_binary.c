a6d97776b703d4b671557ea2c5b34e2e72c91536
Omar <138858455+sk1nsan@users.noreply.github.com>
2023-09-28 04:08:08 -0700

    fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,24 +8,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, l = 0;
-	unsigned long int x = n;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	for (i = 0; x; i++)
-	{
-		l++;
-		x = x & ~(1 << i);
-	}
-	for (i = 0; l > 0; l--, i++)
-	{
-		if ((n & (1 << (l - 1))) != 0)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,14 +8,13 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, result = 0;
+	int  result = 0;
 	int x = n ^ m;
 
-	for (i = 0; x; i++)
+	while (x)
 	{
-		if ((x & (1 << i)) != 0)
-			result++;
-		x = x & ~(1 << i);
+		x = x & (x -1);
+		result++;
 	}
 	return (result);
 }
