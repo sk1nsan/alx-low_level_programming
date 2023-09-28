631c4b4fff41586ea746e98584cf70045e11516f
Omar <138858455+sk1nsan@users.noreply.github.com>
2023-09-28 00:00:13 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+/**
+ * print_binary - prints int in binary
+ *
+ * @n: int to be printed
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i, l = 0;
+	unsigned long int x = n;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = 0; x; i++)
+	{
+		l++;
+		x = x & ~(1 << i);
+	}
+	for (i = 0; l > 0; l--, i++)
+	{
+		if ((n & (1 << (l - 1))) != 0)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
