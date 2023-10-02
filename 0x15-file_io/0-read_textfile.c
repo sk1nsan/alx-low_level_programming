9a91214767d843558cdc419331f5bdb2a94619e7
Omar <138858455+sk1nsan@users.noreply.github.com>
2023-10-02 08:50:04 -0700

    read file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - read file
+ *
+ * @filename: pointer to the file
+ * @letters: number of letters to be printed
+ *
+ * Return: number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *c = malloc(sizeof(char) * letters);
+	int fd, x;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	x = read(fd, c, letters);
+	printf("%s", c);
+	close(fd);
+	if (x == -1)
+		return (0);
+	return (x);
+}
