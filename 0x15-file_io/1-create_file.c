42dc228673f2ca4354cd661e904b1b3b3de159bb
Omar <138858455+sk1nsan@users.noreply.github.com>
2023-10-02 10:20:37 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,12 +15,11 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t i, x;
+	int fd, i ,x;
 
 	if (filename == NULL)
-		return (0);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (0);
 	if (text_content == NULL)
