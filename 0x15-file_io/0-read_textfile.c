32abb5eea565765f9617aa2c50c2a0d99a881f87
Omar <138858455+sk1nsan@users.noreply.github.com>
2023-10-02 09:29:06 -0700

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,8 +23,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
 		free(c);
 		return (0);
+	}
 	x = read(fd, c, letters);
 	if (x == -1)
 	{
