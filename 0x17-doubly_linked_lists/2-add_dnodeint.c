52820b7d9c537885f13e61e8beea1bca63c312cb
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-07 16:10:28 +0200

    fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds an element to the linked list
+ * add_dnodeint - adds an element at the beginning of the linked list
  *
  * @head: first node
  * @n: int to be copied
@@ -14,10 +14,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (!head)
+	if (head == NULL)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 	new->next = *head;
 	new->prev = NULL;
