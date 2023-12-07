2a06d4cffac16d9671f3ae651779ce632552db55
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-07 16:15:02 +0200

    fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,10 +19,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+	if (*head == NULL)
+	{
+
+	}
 	new->next = *head;
 	new->prev = NULL;
 	new->n = n;
-	(*head)->prev = new;
+	if (*head)
+		(*head)->prev = new;
 	*head = new;
 	return (new);
 }
