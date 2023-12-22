d48ce4c3b905b168275411024f619d51f95da96e
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-22 15:26:53 +0200

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,12 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * hash_table_set - set the hashnode to its index in hash table
+ *
+ * @ht: hashtable
+ * @key: key of the hashnode
+ * @value: value of hashnode
+ *
+ * Return: 1 if succ
+ * 0 otherwise
+*/
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *hash_node;
 
-	if (!ht || ht->size == 0)
+	if (ht == NULL)
+		return (0);
+	if (ht->size == 0)
 		return (0);
 	if (key[0] == '\0')
 		return (0);
