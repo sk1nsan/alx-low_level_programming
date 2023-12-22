2d6471ca7e0968f782a0b6e7a624a63fff028449
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-22 17:06:58 +0200

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
  *
  * Return: 1 if succ
  * 0 otherwise
- */
+*/
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -19,11 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *hash_node;
 	hash_node_t *curr;
 
-	if (ht == NULL)
-		return (0);
-	if (ht->size == 0)
-		return (0);
-	if (key[0] == '\0')
+	if (!ht || ht->size == 0 || key[0] == '\0')
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	curr = ht->array[index];
@@ -33,19 +29,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(curr->value);
 			curr->value = strdup(value);
-			if (!curr->value)
-				return (0);
 			return (1);
 		}
 		curr = curr->next;
 	}
-
 	hash_node = malloc(sizeof(hash_node_t));
 	if (hash_node == NULL)
 		return (0);
-
 	hash_node->key = (char *)key;
 	hash_node->value = strdup(value);
+	if (hash_node->value)
+	{
+		free(hash_node->value);
+		free(hash_node);
+		return (0);
+	}
 
 	if ((ht->array[index]) == NULL)
 	{
