d660d20701ca18ca7e8b6da0d0efe4f6167c2ee4
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-22 15:55:19 +0200

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,10 +26,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (key[0] == '\0')
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	hash_node = malloc(sizeof(hash_node_t));
-	if (!hash_node)
-		return (0);
-
 	curr = ht->array[index];
 	while (curr)
 	{
@@ -42,10 +38,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		curr = curr->next;
 	}
 
+	hash_node = malloc(sizeof(hash_node_t));
+	if (!hash_node)
+		return (0);
+
 	hash_node->key = (char *)key;
 	hash_node->value = strdup(value);
 
-	if (!(ht->array[index]))
+	if ((ht->array[index]) == NULL)
 	{
 		ht->array[index] = hash_node;
 		hash_node->next = NULL;
